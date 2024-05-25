1d1c287626444bd4b1fc4d2b7b0d68f54f78daf6
anshulsoni4 <anshulsoni3040@gmail.com>
2024-05-26 00:15:45 +0530

    feat: Hackathon and Tech Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Page = () => {
-  return <div className={"text-center"}>Hello World</div>;
+  return <div className=  {"text-center"}>Team Sync</div>;
 };
 
 export default Page;
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,9 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 } satisfies Config
 
 export default config
